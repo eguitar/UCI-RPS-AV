f99e354712a390b043c82b721d88a2278d6a22c3
eguitar <eguitarrocks@yahoo.com>
2024-05-09 16:50:39 -0700

    added revised rf test with new sensor

--- RF/rf_transmit_full_v2/rf_transmit_full_v2/rf_transmit_full_v2.ino
+++ RF/rf_transmit_full_v2/rf_transmit_full_v2/rf_transmit_full_v2.ino
@@ -5,6 +5,7 @@
 #include <Adafruit_LIS3MDL.h>
 #include "Adafruit_BMP3XX.h"
 #include <Adafruit_LSM6DS3TRC.h>
+#include <Adafruit_LIS3DH.h>
 #include <SoftwareSerial.h>
 // ----------------------------
 #define main_1 7     // main primary
@@ -16,15 +17,18 @@
 // ----------------------------
 Adafruit_BMP3XX bmp;
 Adafruit_LIS3MDL mdl;
-// Adafruit_LSM6DS3TRC lsm;
+Adafruit_LSM6DS3TRC lsm;
+Adafruit_LIS3DH lis = Adafruit_LIS3DH();
 SoftwareSerial mySerial(0,1); // RX, TX
 // ----------------------------
-const int delay_time = 1000;
+const int delay_time = 500;
 const int charge_delay = 500;
 const int backup_delay = 2500;
 bool launch_flag;
 bool drogue_flag;
 bool main_flag;
+bool lis3mdl;
+bool lis3dh;
 int fall_counter;
 float pre_alt;
 
@@ -43,7 +47,7 @@ void writeSD(String data) {
 }
 
 void setup() {
-  tone(buzzer, 3000, 5000);
+  tone(buzzer, 3000, 3000);
 
   // Serial.print("FLIGHT COMPUTER ON\n");
 
@@ -58,7 +62,7 @@ void setup() {
   // Serial.print("SD-CARD initialized.\n");
 
   // Serial.print("Connecting to BMP3XX...");
-  if (!bmp.begin_I2C(0x77, &Wire2)) {
+  if (!bmp.begin_I2C()) {
     // Serial.print("sensor not found, check wiring!\n");
     writeSD("BMP3XX not found");
     exit(0);
@@ -66,7 +70,7 @@ void setup() {
   // Serial.print("BMP3XX found.\n");
 
   // Serial.print("Connecting to LSM6DS3TR-C...");
-  if (!lsm.begin_I2C(0x6A, &Wire2)) {
+  if (!lsm.begin_I2C()) {
     // Serial.print("sensor not found, check wiring!\n");
     writeSD("LSM6DS3TR-C not found");
     exit(0);
@@ -75,13 +79,23 @@ void setup() {
 
   lis3mdl = true;
   // Serial.print("Connecting to LIS3MDL...");
-  if (!mdl.begin_I2C(0x1C, &Wire2)) {
+  if (!mdl.begin_I2C()) {
     Serial.print("sensor not found, check wiring!\n");
     lis3mdl = false;
     writeSD("LIS3MDL not found");
     // exit(0);
   }
   // Serial.print("LIS3MDL found.\n");
+
+  lis3dh = true;
+  // Serial.print("Connecting to LIS3DH...");
+  if (!lis.begin(0x18)) {
+    Serial.print("sensor not found, check wiring!\n");
+    lis3dh = false;
+    writeSD("LIS3DH not found");
+    // exit(0);
+  }
+  // Serial.print("LIS3DH found.\n");  
   
   Serial.print("Altitude, Temperature, Pressure, Acceleration [X, Y, Z] (m/s^2), Orientation [X, Y, Z] (rad/s), Magnetic Field [X, Y, Z] (uTesla):\n");
 
@@ -101,55 +115,58 @@ float alt, temp, pres;
 float acc_x, acc_y, acc_z;
 float gyro_x, gyro_y, gyro_z;
 float mag_x, mag_y, mag_z;
+float acc_x_2, acc_y_2, acc_z_2;
 
 void loop() {
 
   // tone(buzzer, 2000, 1000); // comment out for actual flight
   
-  // if (! bmp.performReading()) {
-  //   Serial.println("BMP failed to perform reading.\n");
-  //   Serial.println("### -- Flight Computer Crashed. -- ###\n");
-  //   exit(0);
-  // }
-
-    alt = 0;
-    temp = 0;
-    pres = 0;
+  if (! bmp.performReading()) {
+    Serial.println("BMP failed to perform reading.\n");
+    Serial.println("### -- Flight Computer Crashed. -- ###\n");
+    exit(0);
+  }
   
-  // alt = bmp.readAltitude(SEALEVELPRESSURE_HPA);
-  // temp = bmp.temperature;
-  // pres = bmp.pressure / 100.0;
+  alt = bmp.readAltitude(SEALEVELPRESSURE_HPA);
+  temp = bmp.temperature;
+  pres = bmp.pressure / 100.0;
 
-  // sensors_event_t acc, gyro, tmp; 
-  // lsm.getEvent(&acc, &gyro, &tmp);
+  sensors_event_t acc, gyro, tmp; 
+  lsm.getEvent(&acc, &gyro, &tmp);
 
-  // acc_x = acc.acceleration.x;
-  // acc_y = acc.acceleration.y;
-  // acc_z = acc.acceleration.z;
+  acc_x = acc.acceleration.x;
+  acc_y = acc.acceleration.y;
+  acc_z = acc.acceleration.z;
 
-  // gyro_x = gyro.gyro.x;
-  // gyro_y = gyro.gyro.y;
-  // gyro_z = gyro.gyro.z;
-
-  acc_x = 0;
-  acc_y = 0;
-  acc_z = 0;
-
-  gyro_x = 0;
-  gyro_y = 0;
-  gyro_z = 0;
+  gyro_x = gyro.gyro.x;
+  gyro_y = gyro.gyro.y;
+  gyro_z = gyro.gyro.z;
   
-  // sensors_event_t mag;
-  // mdl.getEvent(&mag);
+  if (lis3mdl) {
+    sensors_event_t mag;
+    mdl.getEvent(&mag);
+    mag_x = mag.magnetic.x;
+    mag_y = mag.magnetic.y;
+    mag_z = mag.magnetic.z;
+  }
+  else {
+    mag_x = 0.0;
+    mag_y = 0.0;
+    mag_z = 0.0;
+  }
 
-  // mag_x = mag.magnetic.x;
-  // mag_y = mag.magnetic.y;
-  // mag_z = mag.magnetic.z;
-
-  mag_x = 0;
-  mag_y = 0;
-  mag_z = 0;
- 
+  if (lis3dh) {
+    sensors_event_t acel;
+    lis.getEvent(&acel);
+    acc_x_2 = acel.magnetic.x;
+    acc_y_2 = acel.magnetic.y;
+    acc_z_2 = acel.magnetic.z;
+  }
+  else {
+    acc_x_2 = 0.0;
+    acc_y_2 = 0.0;
+    acc_z_2 = 0.0;
+  }
 
   // {datalogging} ------------------------------------
   String dataString = String(alt) + "," + 
@@ -163,20 +180,16 @@ void loop() {
                       String(gyro_z) + "," +
                       String(mag_x) + "," +
                       String(mag_y) + "," +
-                      String(mag_z);
+                      String(mag_z) + "," +
+                      String(acc_x_2) + "," +
+                      String(acc_y_2) + "," +
+                      String(acc_z_2);
                       
   Serial.println(dataString);
   mySerial.println(dataString);
   
-  // File dataFile = SD.open("data_log.csv", FILE_WRITE);
+  writeSD(dataString);
   
-  // if (dataFile) {
-  //   dataFile.println(dataString);
-  //   dataFile.close();
-  // }
-  // else {
-  //   Serial.println("Error Opening Data File.\n");
-  // }
   // --------------------------------------------------
   delay(delay_time);
 }
