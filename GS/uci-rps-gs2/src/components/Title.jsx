67cc8feea178d0185a06fa6fa131f43af951ba52
eguitar <eguitarrocks@yahoo.com>
2024-06-15 02:08:54 -0700

    remove redundant gs

--- GS/uci-rps-gs2/src/components/Title.jsx
+++
@@ -1,8 +0,0 @@
-function Title() {
-    return (
-      <div className="title">
-          <h1 style={{ fontSize: '2.5em' }}>UCI Rocket Project Solids - Ground Station</h1>
-      </div>
-    );
-  }
-  export default Title;
